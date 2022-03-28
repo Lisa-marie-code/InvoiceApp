2088429502ff67bc829b2fa575f7e68bcc3e71a6
Lisa-marie-code <lisamariekoomson340@gmail.com>
2022-03-28 11:00:21 +0000

    filer bar

--- index.js
+++ index.js
@@ -4,7 +4,20 @@ const colorTheme = document.getElementById("#color_mode");
 
 function toggleTheme() {
   document.body.classList.toggle("dark");
+
 }
 colorTheme.addEventListener("click", toggleTheme);
 
+//filtering bar
+var expanded = false;
 
+function showCheckboxes() {
+  var checkboxes = document.getElementById("checkboxes");
+  if (!expanded) {
+    checkboxes.style.display = "block";
+    expanded = true;
+  } else {
+    checkboxes.style.display = "none";
+    expanded = false;
+  }
+}
