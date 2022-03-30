500ad71fd1c8963de25419e0255785b2ebba40f3
Asante-Nsiah <88829934+Asante-Nsiah@users.noreply.github.com>
2022-03-30 09:57:37 +0000

    data styled

--- script.js
+++ script.js
@@ -13,8 +13,31 @@ $(document).ready(function(){
             code.className = 'idenNum';
             code.innerText = value.id;
             invoiceCard.appendChild(code);
-            
-           
+
+           let due = document.createElement('div');
+           due.className = 'dueDate';
+           due.innerText = value.paymentDue;
+           invoiceCard.appendChild(due);
+
+           let amount = document.createElement('div');
+           amount.className = 'amt';
+           amount.innerText = value.total;
+           invoiceCard.appendChild(amount);
+
+        //    let name = document.createElement('div');
+        //    name.id = 'buttonOne';
+        //    invoiceCard.appendChild(name);
+
+           let nameStatus = document.createElement('div');
+           nameStatus.className = 'client';
+           nameStatus.innerText = value.clientName;
+           invoiceCard.appendChild(nameStatus);
+
+           let pay = document.createElement('button');
+           pay.className = 'payment';
+           pay.innerText = value.status;
+           invoiceCard.appendChild(pay);
+
 
             
             
