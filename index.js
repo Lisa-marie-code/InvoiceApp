1d3dd34de5d42f57b14d00e103412430ac42a2b0
Lisa-marie-code <lisamariekoomson340@gmail.com>
2022-03-30 12:29:25 +0000

    merged files

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 // changing body theme to dark
 const body = document.querySelector("body");
 const colorTheme = document.getElementById("#color_mode");
@@ -9,7 +10,7 @@ function toggleTheme() {
   moon_mode.style.display = 'none';
 
 }
-colorTheme.addEventListener("click", toggleTheme);
+// colorTheme.addEventListener("click", toggleTheme);
 
 //filtering bar
 var expanded = false;
@@ -24,3 +25,61 @@ function showCheckboxes() {
     expanded = false;
   }
 }
+
+// json
+let main_container = document.createElement('div');
+main_container.className = 'mcontainer';
+document.body.appendChild(main_container);
+
+
+$(document).ready(function(){
+  $.getJSON("data.json", function(data){
+      $.each(data, function(index, value){ 
+          console.log(value);
+          
+          let invoiceCard = document.createElement('div');
+          invoiceCard.className = 'card';
+          invoiceCard.id = 'tickect';
+          // document.body.appendChild(invoiceCard);
+          main_container.appendChild(invoiceCard);
+
+          let code = document.createElement('div');
+          code.className = 'idenNum';
+          code.innerText = value.id;
+          invoiceCard.appendChild(code);
+
+         let due = document.createElement('div');
+         due.className = 'dueDate';
+         due.innerText = value.paymentDue;
+         invoiceCard.appendChild(due);
+
+         let amount = document.createElement('div');
+         amount.className = 'amt';
+         amount.innerText = value.total;
+         invoiceCard.appendChild(amount);
+
+      //    let name = document.createElement('div');
+      //    name.id = 'buttonOne';
+      //    invoiceCard.appendChild(name);
+
+         let nameStatus = document.createElement('div');
+         nameStatus.className = 'client';
+         nameStatus.innerText = value.clientName;
+         invoiceCard.appendChild(nameStatus);
+
+         let pay = document.createElement('button');
+         pay.className = 'payment';
+         pay.innerText = value.status;
+         invoiceCard.appendChild(pay);
+
+
+          
+          
+              
+             
+      });
+
+
+      });
+  });
+
