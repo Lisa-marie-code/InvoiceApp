adbbaef4281706f1cd280ad5554ea0d53e32521b
Lisa-marie-code <lisamariekoomson340@gmail.com>
2022-04-12 16:15:34 +0000

    added arrows and status points

--- index.js
+++ index.js
@@ -75,8 +75,13 @@ $(document).ready(function(){
          
          let round = document.createElement('span');
          round.className = 'roundStatus';
+         round.style.backgroundColor = value.hex;
          pay.appendChild(round);
 
+         let arrow = document.createElement('div');
+         arrow.className = 'arrowImage';
+         arrow.innerHTML = `<svg width="7" height="10" xmlns="http://www.w3.org/2000/svg"><path d="M1 1l4 4-4 4" stroke="#7C5DFA" stroke-width="2" fill="none" fill-rule="evenodd"/></svg>`;
+         invoiceCard.appendChild(arrow);
         
       });
       
