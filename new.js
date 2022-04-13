228f5799f327a2ff421387f10aab2117c0fe821a
Lisa-marie-code <lisamariekoomson340@gmail.com>
2022-04-13 16:57:16 +0000

    create new form

--- new.js
+++ new.js
@@ -22,7 +22,21 @@ create_Container.appendChild(formHolder);
 let form = document.createElement("form");
     form.setAttribute("method", "post");
     form.setAttribute("action", "submit");
+    form.className = 'newForm';
     formHolder.appendChild(form);
+
+    let billfrom = document.createElement('p');
+    billfrom.innerHTML = 'Bill From';
+    billfrom.style.color = '#7C5DFA';
+    billfrom.style.marginLeft = '20px';
+    formHolder.appendChild(billfrom);
+
+    
+    let newlabel = document.createElement("label");
+    newlabel.setAttribute("for",'StreetAddress');
+    newlabel.innerHTML = "Street Address";
+    newlabel.style.marginLeft = '20px';
+    formHolder.appendChild(newlabel);
 
     let Staddress = document.createElement("input");
     Staddress.setAttribute("type", "text");
@@ -30,9 +44,73 @@ let form = document.createElement("form");
     Staddress.className = 'StreetAddress';
     formHolder.appendChild(Staddress);
 
-    let newlabel = document.createElement("Label");
-    newlabel.setAttribute("for",'StreetAddress');
-    newlabel.innerHTML = "Street Address";
-    formHolder.appendChild(newlabel);
 
-   
+    let city = document.createElement("input");
+   city.setAttribute("type", "text");
+    city.setAttribute("name", "city");
+    city.className = 'City';
+    formHolder.appendChild(city);
+
+    let postcode = document.createElement("input");
+    postcode.setAttribute("type", "text");
+    postcode.setAttribute("name", "postcode");
+    postcode.className = 'PostCode';
+    formHolder.appendChild(postcode);
+
+    let country = document.createElement("input");
+    country.setAttribute("type", "text");
+    country.setAttribute("name", "country");
+    country.className = 'Country';
+    formHolder.appendChild(country);
+
+    let billto = document.createElement('p');
+    billto.innerHTML = 'Bill To';
+    billto.style.color = '#7C5DFA';
+    billto.style.marginLeft = '20px';
+    billto.style.marginTop = '40px';
+    formHolder.appendChild(billto);
+
+    let labelOne = document.createElement("label");
+    labelOne.setAttribute("for",'Clients Name');
+    labelOne.innerHTML = "Client's name";
+    labelOne.style.marginLeft = '20px';
+    formHolder.appendChild(labelOne);
+
+    let Cname = document.createElement("input");
+    Cname.setAttribute("type", "text");
+    Cname.setAttribute("name", "Client's Name");
+    Cname.className = 'CName';
+    formHolder.appendChild(Cname);
+
+    let labelTwo = document.createElement("label");
+    labelTwo.setAttribute("for",'Clients Email');
+    labelTwo.innerHTML = "Client's Email";
+    labelTwo.style.marginLeft = '20px';
+    formHolder.appendChild(labelTwo);
+
+    let Cemail = document.createElement("input");
+    Cemail.setAttribute("type", "text");
+    Cemail.setAttribute("name", "Client's Email");
+    Cemail.setAttribute("placeholder", "e.g.email@example.com");
+    Cemail.className = 'cemail';
+    formHolder.appendChild(Cemail);
+
+    let cityOne = document.createElement("input");
+    cityOne.setAttribute("type", "text");
+     cityOne.setAttribute("name", "city1");
+     cityOne.className = 'City_one';
+     formHolder.appendChild(cityOne);
+ 
+     let postcodeOne = document.createElement("input");
+     postcodeOne.setAttribute("type", "text");
+     postcodeOne.setAttribute("name", "postcode1");
+     postcodeOne.className = 'PostCode_one';
+     formHolder.appendChild(postcodeOne);
+ 
+     let countryOne = document.createElement("input");
+     countryOne.setAttribute("type", "text");
+     countryOne.setAttribute("name", "country1");
+     countryOne.className = 'Country_one';
+     formHolder.appendChild(countryOne);
+
+    
