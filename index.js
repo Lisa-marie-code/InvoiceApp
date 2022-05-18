ccbccb44692539ec21e8997a04a6f4338cf529f0
Asante-Nsiah <88829934+Asante-Nsiah@users.noreply.github.com>
2022-05-18 00:35:17 +0000

    Updated ticket details 2

--- index.js
+++ index.js
@@ -114,38 +114,121 @@ $(document).ready(function(){
       stat.style.color = value.hex;
     
       statusDivContainer.appendChild(stat);
+
+      let group1 = document.createElement('div');
+      group1.className = 'group1';
+      statusDiv.appendChild(group1);
        
       let dtCode = document.createElement('div');
       dtCode.className = 'dtidenNum';
       dtCode.innerText = '#' +  value.id;
-      statusDiv.appendChild(dtCode);
+      dtCode.id = 'bold';
+      group1.appendChild(dtCode);
 
       let dtdescript = document.createElement('div');
       dtdescript.className = 'dtidenNum';
       dtdescript.innerText = value.description;
-      statusDiv.appendChild(dtdescript);
+      group1.appendChild(dtdescript);
+
+      let group2 = document.createElement('div');
+      group2.className = 'group2';
+      group2.innerText = value.senderAddress.street;
+      statusDiv.appendChild(group2);
+
+      let dtstreet =document.createElement('div');
+      dtstreet.className = 'dtstreetNum';
+      group2.appendChild(dtstreet);
 
       let dtcity = document.createElement('div');
       dtcity.className = 'dtidenNum';
       dtcity.innerText = value.senderAddress.city;
-      statusDiv.appendChild(dtcity);
+      group2.appendChild(dtcity);
 
       let dtpostcode = document.createElement('div');
       dtpostcode.className = 'dtidenNum';
       dtpostcode.innerText = value.senderAddress.postCode;
-      statusDiv.appendChild(dtpostcode);
+      group2.appendChild(dtpostcode);
 
       let dtcountry = document.createElement('div');
       dtcountry.className = 'dtidenNum';
       dtcountry.innerText = value.senderAddress.country;
-      statusDiv.appendChild(dtcountry);
+      group2.appendChild(dtcountry);
+
+      
+      const group5 = document.createElement('div');
+      group5.className = 'group5';
+      statusDiv.appendChild(group5);
+
+      let group3 = document.createElement('div');
+      group3.className = 'group3';
+      group5.appendChild(group3);
+
+      let invoiceDate = document.createElement('div');
+      invoiceDate.innerText = 'Invoice Date';
+      group3.appendChild(invoiceDate);
 
       let dtcreated = document.createElement('div');
       dtcreated.className = 'dtidenNum';
+      dtcreated.id = 'bold';
       dtcreated.innerText = value.createdAt;
-      statusDiv.appendChild(dtcreated);
+      group3.appendChild(dtcreated);
+
+      let pDue = document.createElement('div');
+      pDue.innerText = 'Payment Due';
+      group3.appendChild(pDue);
+
+      let dueDate = document.createElement('div');
+      dueDate.className = 'dtidenNum';
+      dueDate.id = 'bold';
+      dueDate.innerText = value.paymentDue;
+      group3.appendChild(dueDate);
       
-      
+      let group4 = document.createElement('div');
+      group4.className = 'group4';
+      group5.appendChild(group4);
+
+      let billTo = document.createElement('div');
+      billTo.className = 'dtbillTo';
+      billTo.innerHTML = 'Bill To';
+      group4.appendChild(billTo);
+
+      let clientName = document.createElement('div');
+      clientName.className = 'dtclientName';
+      clientName.id = 'bold';
+      clientName.innerHTML = value.clientName;
+      group4.appendChild(clientName);
+
+      let clientStreet = document.createElement('div');
+      clientStreet.className = 'dtclientstreet';
+      clientStreet.innerHTML = value.clientAddress.street;
+      group4.appendChild(clientStreet);
+
+      let clientCity = document.createElement('div');
+      clientCity.className = 'dtclientstreet';
+      clientCity.innerHTML = value.clientAddress.city;
+      group4.appendChild(clientCity);
+
+      let clientPost = document.createElement('div');
+      clientPost.className = 'dtclientpost';
+      clientPost.innerHTML = value.clientAddress.postCode;
+      group4.appendChild(clientPost);
+
+      let clientCountry = document.createElement('div');
+      clientCountry.className = 'dtclientcountry';
+      clientCountry.innerHTML = value.clientAddress.country;
+      group4.appendChild(clientCountry);
+
+      let sentTo = document.createElement('div');
+      sentTo.className = 'dtclientemail';
+      sentTo.innerText = 'Sent to';
+      statusDiv.appendChild(sentTo);
+
+      let clientEmail = document.createElement('div');
+      clientEmail.className = 'dtclientemail';
+      clientEmail.id = 'bold';
+      clientEmail.innerText = value.clientEmail;
+      statusDiv.appendChild(clientEmail);
+
 
      });
 
